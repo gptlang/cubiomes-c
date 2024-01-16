96ee77bb4199bc547f4b3c166ec09fbbf89bffe5
Antonio Cheong <teapotv8@proton.me>
2024-01-16 03:50:22 +0000

    fix builds for windows and MacOS

---
+++ main.h
@@ -0,0 +1,5 @@
+#include "finders.h"
+int main();
+long long getNearestStructure(int structType, int x, int z, uint64_t seed, int mc);
+long long getNearestStronghold(int x, int z, uint64_t seed, int mc);
+int getDimFromFeature(enum StructureType structType);
